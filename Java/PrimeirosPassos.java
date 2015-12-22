ee9ffa20604110b9a440e3f818ba711957f6dd43
lunatic0696 <lunatic_soul0696@hotmail.com>
2015-12-22 18:32:52 -0200

    Primeiro push

---
+++ Java/PrimeirosPassos.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import javax.swing,JOptionPane
+
+public class PrimeirosPassos {
+	public static void main(String[] args){
+		System.out.println("aaaaaaaaaaaaaaaaaaaaaaaa");
+		
+	}
+}
