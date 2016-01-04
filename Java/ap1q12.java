5d95cb97096be572cb115f3420d6789fb51e1185
lunatic0696 <lunatic_soul0696@hotmail.com>
2016-01-04 12:34:41 -0200

    rsgfgs
    sdfgsdfg

---
+++ Java/ap1q12.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ap1q12{	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		double nota1 = scanner.nextDouble();
+		double nota2 = scanner.nextDouble();
+		double media = 0;
+		if(nota1 > 10 || nota1 < 0){
+			System.out.format("Nota invalida");
+		}
+		if(nota2 > 10 || nota2 < 0){
+			System.out.format("Nota invalida");
+		}
+		else if(nota1 < 10 && nota1 > 0){
+			if(nota2 < 10 && nota2 > 0){
+				media = (double)((nota1+nota2)/2);
+				System.out.format("Media - %.2f", media);
+			}
+		}
+	}
+}
